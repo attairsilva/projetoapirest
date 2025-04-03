0a90ecd7f4b81dbc73c57506b4633ddb727736a7
Attair Silva <attaircba@gmail.com>
2025-04-03 01:42:56 -0400

    Atualiza

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Define o diretório do Laravel
-APP_DIR="/var/www/html"
-
 # Configura o tempo limite para aguardar iniciar o PostgreSQL
 TIMEOUT=30
 TIMER=0
@@ -18,16 +15,11 @@ while ! curl -s http://minio:9000/minio/health/live >/dev/null; do
   fi
 done
 
-echo "MinIO está pronto!"
-
-
-# Acessa o diretório do Laravel
-cd "$APP_DIR"
 # Verifica se o .env já existe, se não, copia o env.exemplo
-if [ ! -f "$APP_DIR/.env" ]; then
-    if [ -f "$APP_DIR/env.exemplo" ]; then
+if [ ! -f ".env" ]; then
+    if [ -f "env.exemplo" ]; then
         echo "Criando arquivo .env..."
-        cp "$APP_DIR/env.exemplo" "$APP_DIR/.env"
+        cp "env.exemplo" ".env"
     else
         echo "Erro: Arquivo env.exemplo não encontrado!"
         exit 1
